bed2dc8f43140825239f94e46bb2ac0914fb278c
phil-zxx <32247590+phil-zxx@users.noreply.github.com>
2019-05-16 00:26:37 +0100

    Minor changes in cmake & sample script file

--- src/sample_script.cpp
+++ src/sample_script.cpp
@@ -5,10 +5,10 @@ LITTLE_ENUM_CLASS(Animal, Cat, Dog, Horse, Rabbit);
 
 int main()
 {
-    constexpr Animal val1 = Animal::Cat;
+    const Animal val1 = Animal::Cat;
     std::cout << "1) This enum prints to '" << val1 << "'" << std::endl;
 
-    constexpr Animal val2 = LittleEnum::fromStr<Animal>("Rabbit");
+    const Animal val2 = LittleEnum::fromStr<Animal>("Rabbit");
     std::cout << "2) This enum prints to '" << LittleEnum::toStr(val2) << "'" << std::endl;
 
     return 0;
